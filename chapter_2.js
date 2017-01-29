125e21353f85c892b6d35f5566fe742f0b2c0518
Marcela Silva <marcelabsr@me.com>
2017-01-29 11:18:32 -0500

    all exercises chapter 2

--- chapter_2.js
+++ chapter_2.js
@@ -31,5 +31,47 @@ console.log(isEven(-1))
 
 
 // 3.
+function countBs (string) {
+  var counter = 0;
+  for (var i = 0; i < string.length; i++){
+    if (string[i] === "B") {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+console.log(countBs("BABYBABYBABY"))
+
+// 3.1
+function countChar (string, char) {
+  var counter = 0;
+  for (var i = 0; i < string.length; i++){
+    if (string[i] === char) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+console.log(countChar("krispykreme", "k"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
