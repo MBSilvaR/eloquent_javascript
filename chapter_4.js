88f53de1149ae9add0bbe9e99534bfdaa013969c
Marcela Silva <marcelabsr@me.com>
2017-02-02 10:26:39 -0800

    chapter 4 2.1

--- chapter_4.js
+++ chapter_4.js
@@ -42,3 +42,19 @@ function reverseArray(array) {
   return output;
 }
 console.log(reverseArray([1,2,3]))
+
+//2.1
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+
